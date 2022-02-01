a1952c73162fb5fc25408b84f9c06b399eabbe51
Karag <karag2006@gmx.net>
2022-02-01 14:36:43 +0100

    Erste Version funktioniert vollständig.

--- update.sh
+++ update.sh
@@ -13,6 +13,21 @@ getComputerValues () {
     IFS=',' read -r -a computerValues <<< "$1"
 }
 
+runUpdate () {
+    # function requires $ip,$port,$userName and $packageSystem to be set before running.
+    scp -P $port ./localUpdate.sh "$ip":/home/"$userName"/localUpdate.sh
+
+    echo "Running on $name with user $userName:"
+
+    if [ $packageSystem == "apt" ]
+    then
+        ssh $userName@$ip -p $port -t 'bash -l -c "./localUpdate.sh apt;bash"'
+    elif [ $packageSystem == "paru" ]
+    then
+        ssh $userName@$ip -p $port -t 'bash -l -c "./localUpdate.sh paru;bash"'
+    fi
+}
+
 # handle arguments
 # for now n == name is the only valid option
 # if option -n is provided the following argument should be the valid name of a PC from Computers
@@ -56,19 +71,22 @@ done
 if [ ${#computerArray[*]} -eq $numberOfOptions ]
 # this means that only 1 computer is in the array (a valid name was provided)
 then
+    name=${computerArray[0,0]}
     ip=${computerArray[0,1]}
     port=${computerArray[0,2]}
     userName=${computerArray[0,3]}
     packageSystem=${computerArray[0,4]}
 
-    scp -P $port /home/martin/code/bash/projects/updates/localUpdate.sh "$ip":/home/"$userName"/localUpdate.sh
+    runUpdate
+else
+    # app needs to run through the entire computer list.
+    for (( i=0; i<$numberOfComputers; i++ )); do
+        name=${computerArray[$i,0]}
+        ip=${computerArray[$i,1]}
+        port=${computerArray[$i,2]}
+        userName=${computerArray[$i,3]}
+        packageSystem=${computerArray[$i,4]}
 
-    if [ $packageSystem == "apt" ]
-    # this means that only 1 computer is in the array (a valid name was provided)
-    then
-        ssh $userName@$ip -p $port -t 'bash -l -c "./localUpdate.sh apt;bash"'
-    elif [ $packageSystem == "paru" ]
-    then
-        ssh $userName@$ip -p $port -t 'bash -l -c "./localUpdate.sh paru;bash"'
-    fi
+        runUpdate
+    done
 fi
